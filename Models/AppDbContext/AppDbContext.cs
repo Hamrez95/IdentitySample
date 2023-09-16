1ec24ff5471493bf93522a5398f80476ba5bbf17
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-09-17 01:39:59 +0330

    EF installed , Connection String Connected

---
+++ Models/AppDbContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IdentitySample.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentitySample.Models.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-S136OOG;Initial Catalog=IdentitySampleDB;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        #region DbSets
+        public DbSet<Employee> Employees { get; set; }
+        #endregion
+    }
+}
---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentitySample.Models.Entities
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string FirstNamme { get; set; }
+        public string LastNamme { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+    }
+}
