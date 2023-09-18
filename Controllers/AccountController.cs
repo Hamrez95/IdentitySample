7abe1d801ae114d5f866bba25e34eb7d997d90f5
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-09-18 19:28:30 +0330

    RegisterViewModel and Account Controller & view Created

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentitySample.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name ="Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
